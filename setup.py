70e192b2c99ffa36f1827cb868c99a3fe62d6be5
Herman Caldara <hermancaldara@gmail.com>
2015-05-12 21:31:17 -0300

    Updates setup.py.

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup
 setup(
     name='2112',
     version='0.1.0',
+    description="Shows 2112' starman on terminal.",
+    author='Herman Caldara',
+    author_email='hermancaldara@gmail.com',
+    url='https://github.com/hermancaldara/2112',
+    license='MIT',
     scripts=['2112']
 )
